555baf02afe81e588539e733714cd8ef7b09d4f5
anton8900 <antonolegovich12@gmail.com>
2015-09-26 01:07:27 -0700

    Initial Commit

---
+++ Lab2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lab2
+//
+//  Created by Anton Skutov on 9/26/15.
+//  Copyright (c) 2015 Anton Skutov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
